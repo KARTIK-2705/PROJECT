f291e46e52a123e73c9fefc7357632943ded565b
KARTIK-2705 <kartiksutar53@gmail.com>
2024-09-26 22:31:07 +0530

    1ST COMMIT
    1ST COMMIT

---
+++ index.js
@@ -0,0 +1,26 @@
+function login(){
+    let username= document.getElementById("username")
+    let password2= document.getElementById("password2")
+
+    let website_username="KARTIK";
+    let website_password="12345";
+
+    if(username==website_username && password2==website_password){
+        window.location="Machinery.html"
+    }
+}
+function func(){
+    var username= document.getElementById("username").value;
+    var password2= document.getElementById("password").value;
+
+    var website_username="KARTIK";
+    var website_password="12345";
+
+    if(username=="KARTIK" && password2=="12345"){
+        window.location.href="Machinery.html";
+        alert("success")
+    }
+    else{
+        alert("Bhai galat entry ki hai")
+    }
+} 
